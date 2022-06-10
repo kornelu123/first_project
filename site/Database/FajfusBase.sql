ce465004b371ede81f0d78ae97a9f5b6f3d7219f
kornelu123 <kornelu12@gmail.com>
2022-06-10 17:48:17 +0200

    login forms

---
+++ site/Database/FajfusBase.sql
@@ -0,0 +1,244 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.5deb2
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Generation Time: Jun 10, 2022 at 04:30 PM
+-- Server version: 8.0.29-0ubuntu0.20.04.3
+-- PHP Version: 7.4.3
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `FajfusBase`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `coments`
+--
+
+CREATE TABLE `coments` (
+  `id` int NOT NULL,
+  `user` int NOT NULL,
+  `post` int NOT NULL,
+  `content` varchar(100) NOT NULL,
+  `lang` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `coments`
+--
+
+INSERT INTO `coments` (`id`, `user`, `post`, `content`, `lang`) VALUES
+(1, 1, 1, 'wiadomo', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `groups`
+--
+
+CREATE TABLE `groups` (
+  `id` int NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `shortcut` varchar(5) NOT NULL,
+  `logo` varchar(50) DEFAULT NULL,
+  `color` varchar(50) NOT NULL,
+  `lang` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `groups`
+--
+
+INSERT INTO `groups` (`id`, `name`, `shortcut`, `logo`, `color`, `lang`) VALUES
+(1, 'Faszista', 'F', NULL, 'red', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `lang`
+--
+
+CREATE TABLE `lang` (
+  `id` int NOT NULL,
+  `lang` varchar(40) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `lang`
+--
+
+INSERT INTO `lang` (`id`, `lang`) VALUES
+(1, 'english'),
+(2, 'Русский'),
+(3, '中國人');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `posts`
+--
+
+CREATE TABLE `posts` (
+  `id` int NOT NULL,
+  `title` varchar(150) NOT NULL,
+  `content` longtext NOT NULL,
+  `user` int NOT NULL,
+  `lang` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `posts`
+--
+
+INSERT INTO `posts` (`id`, `title`, `content`, `user`, `lang`) VALUES
+(1, 'dlaczego polska to est jebana', 'a szkoda gadać', 1, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int NOT NULL,
+  `username` varchar(25) NOT NULL,
+  `password` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `email` varchar(50) DEFAULT NULL,
+  `squad` int DEFAULT NULL,
+  `lang` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `password`, `email`, `squad`, `lang`) VALUES
+(1, 'Patrik', 'chuj', NULL, 1, 1),
+(3, 'asdf', 'asdf12', NULL, 1, 1),
+(7, 'asdf1234', '$2y$10$S8Cg4hDf3e/jvzXIm8Be.OMyoC6OBs8vhzQBrutvmVc2gPuVblOK2', NULL, 1, 1);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `coments`
+--
+ALTER TABLE `coments`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `post` (`post`),
+  ADD KEY `lang` (`lang`),
+  ADD KEY `user` (`user`);
+
+--
+-- Indexes for table `groups`
+--
+ALTER TABLE `groups`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `lang` (`lang`);
+
+--
+-- Indexes for table `lang`
+--
+ALTER TABLE `lang`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `posts`
+--
+ALTER TABLE `posts`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user` (`user`),
+  ADD KEY `lang` (`lang`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `users_ibfk_1` (`lang`),
+  ADD KEY `squad` (`squad`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `coments`
+--
+ALTER TABLE `coments`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `groups`
+--
+ALTER TABLE `groups`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `lang`
+--
+ALTER TABLE `lang`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `posts`
+--
+ALTER TABLE `posts`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `coments`
+--
+ALTER TABLE `coments`
+  ADD CONSTRAINT `coments_ibfk_1` FOREIGN KEY (`post`) REFERENCES `posts` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT,
+  ADD CONSTRAINT `coments_ibfk_2` FOREIGN KEY (`lang`) REFERENCES `lang` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT,
+  ADD CONSTRAINT `coments_ibfk_3` FOREIGN KEY (`user`) REFERENCES `users` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+
+--
+-- Constraints for table `groups`
+--
+ALTER TABLE `groups`
+  ADD CONSTRAINT `groups_ibfk_1` FOREIGN KEY (`lang`) REFERENCES `lang` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+
+--
+-- Constraints for table `posts`
+--
+ALTER TABLE `posts`
+  ADD CONSTRAINT `posts_ibfk_1` FOREIGN KEY (`user`) REFERENCES `users` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT,
+  ADD CONSTRAINT `posts_ibfk_2` FOREIGN KEY (`lang`) REFERENCES `lang` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+
+--
+-- Constraints for table `users`
+--
+ALTER TABLE `users`
+  ADD CONSTRAINT `users_ibfk_1` FOREIGN KEY (`lang`) REFERENCES `lang` (`id`) ON UPDATE CASCADE,
+  ADD CONSTRAINT `users_ibfk_2` FOREIGN KEY (`squad`) REFERENCES `groups` (`id`) ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
--- site/html/cwel.js
+++
@@ -1,8 +0,0 @@
-var cwel
-
-function cwell(){
-    cwel = (document.forms["cwel"]["waga"].value * document.forms["cwel"]["iq"].value)/document.forms["cwel"]["wzrost"].value
-    document.getElementById("wynik").innerHTML = "Twoj wynik cwela to : "+cwel
-}
-
-document.getElementById("butt").addEventListener("click",() =>{cwell()})

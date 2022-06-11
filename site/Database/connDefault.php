1932339ff4e9484bc35b3c7db391c7adada91293
Nelutato <purepeper@gmail.com>
2022-06-11 19:02:40 +0200

    ignore plz

--- site/Database/connDefault.php
+++ site/Database/connDefault.php
@@ -2,7 +2,7 @@
     $host = 'localhost';
     $dbname = 'FajfusBase';
     $username = 'root';
-    $password = 'chuj';
+    $password = 'chu';
 
     try {
         $conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);

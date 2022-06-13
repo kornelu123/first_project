bfdc74ce856f523b7a790c53da2c1c0010990012
kornelu123 <kornelu12@gmail.com>
2022-06-13 11:27:31 +0200

    same thing

--- site/html/en_site.php
+++ site/html/en_site.php
@@ -1,11 +1,10 @@
+<!DOCTYPE html>
 <?php
     require '../Controllers/Global/Auth.php';
     require '../Controllers/posts.php';
     $posts = new Posts;
     $posts = $posts->show();
 ?>
-
-<!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -16,6 +15,7 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Koulen&family=Montserrat&display=swap" rel="stylesheet"> 
+    <script src="../scripts/readMore.js"></script>
 </head>
 <body class=" font-classicFont bg-basicDark">
     <nav id="cwel" class="w-full h-fit bg-basicDark text-basicWhite grid items-center mb-4">
@@ -24,8 +24,8 @@
                     if(Auth::authorised()){
                         $user = $_COOKIE['user'];
                         echo 'You are logged in as <span class="text-darkerWhite">'.$user.'</span>';
+                        echo '<script>isLogged = true</script>';
                     }
-                    
                 ?>
         </h2>
         <h2 class="absolute top-4 right-4 text-xl">
@@ -33,7 +33,7 @@
             <a href="loginForm.php" class="bg-darkViolet hover:text-lighterWhite hover:bg-basicViolet px-4 py-2 border-2 border-basicWhite rounded-b-lg m-4">Log-in</a>
             <a href="registerForm.php" class="bg-darkViolet hover:text-lighterWhite hover:bg-basicViolet px-4 py-2 border-2 border-basicWhite rounded-bl-lg">Sign-in</a>
         </h2>
-    <div class=" flex justify-center items-center m-12 mt-20">
+    <div class=" flex justify-center items-center m-auto mt-20 mb-12">
         <h1 class="text-6xl after:clear-both hover:text-lighterWhite">
             Forum for cwel's
         </h1>
@@ -41,10 +41,10 @@
         <button id="toggle" class="m-4 flex justify-center"><img src="../img/menu.svg" id="button" class="transition-all ease-[cubic-bezier(.32,.82,.89,.39)] w-10 h-10" alt="toggle_menu"></button>
         <menu>
             <ul class="grid h-0 items-cente hover:text-darkerWhite grid-cols-4 transition-all text-slate-200 bg-basicDark absolute w-full overflow-hidden" id="menu">
-                <a href="./en_site.php"><li class="w-full text-center h-fit cursor-pointer hover:scale-125 duration-150 delay-75 hover:z-20 hover:rounded-xl hover:text-lighterWhite overflow-visible py-6 border-r-slate-200 border-r-2 hover:bg-basicViolet hover:border-0 transition-colors ease-in-out block">Home</li></a>
-                <li class="w-full text-center h-fit cursor-pointer hover:scale-125 duration-150 delay-75 hover:z-20 hover:rounded-xl hover:text-lighterWhite overflow-visible py-6 border-r-slate-200 border-r-2 hover:bg-basicViolet hover:border-0 transition-colors ease-in-out block">Cipa</li>
-                <li class="w-full text-center h-fit cursor-pointer hover:scale-125 duration-150 delay-75 hover:z-20 hover:rounded-xl hover:text-lighterWhite overflow-visible py-6 border-r-slate-200 border-r-2 hover:bg-basicViolet hover:border-0 transition-colors ease-in-out block">Pizda</li>
-                <li class="w-full text-center h-fit cursor-pointer hover:scale-125 duration-150 delay-75 hover:z-20 hover:rounded-xl hover:text-lighterWhite overflow-visible py-6 hover:bg-basicViolet hover:border-0 transition-colors ease-in-out block">Chuj</li>
+                <li class="w-full text-center bg-basicDark h-fit cursor-pointer hover:scale-125 duration-150 delay-75 hover:z-20 z-10 hover:rounded-xl hover:text-lighterWhite overflow-visible py-6 border-r-slate-200 border-r-2 hover:bg-basicViolet hover:border-0 transition-colors ease-in-out block"><a href="./en_site.php" class="w-full h-full">Home</a></li>
+                <li class="w-full text-center bg-basicDark h-fit cursor-pointer hover:scale-125 duration-150 delay-75 hover:z-20 z-10 hover:rounded-xl hover:text-lighterWhite overflow-visible py-6 border-r-slate-200 border-r-2 hover:bg-basicViolet hover:border-0 transition-colors ease-in-out block">Cipa</li>
+                <li class="w-full text-center bg-basicDark h-fit cursor-pointer hover:scale-125 duration-150 delay-75 hover:z-20 z-10 hover:rounded-xl hover:text-lighterWhite overflow-visible py-6 border-r-slate-200 border-r-2 hover:bg-basicViolet hover:border-0 transition-colors ease-in-out block">Pizda</li>
+                <li class="w-full text-center bg-basicDark h-fit cursor-pointer hover:scale-125 duration-150 delay-75 hover:z-20 z-10 hover:rounded-xl hover:text-lighterWhite overflow-visible py-6 hover:bg-basicViolet hover:border-0 transition-colors ease-in-out block">Chuj</li>
             </ul>
         </menu>
         <script src="../scripts/menu_script.js"></script>
@@ -55,23 +55,31 @@
                 
             </div>
         </aside>
-        <section class="col-span-7 bg-basicDark h-screen">
-            <div id="main" class="ml-12 bg-basicViolet h-full border-t-4 border-basicWhite">
+        <section class="col-span-7 bg-basicDark h-screen overflow-">
+            <div id="main" class="ml-12 bg-basicViolet h-full border-t-4 opacity-70 border-basicWhite overflow-scroll">
+                <div id="warning" class="text-center pt-10">
+                    
+                </div>
                 <?php foreach ($posts as $post ) { ?>
-                    <div id="post_1" class=" max-h-fit h-64 w-auto m-12 bg-darkerViolet border-y-2 border-basicWhite text-basicWhite">
-                        <?php    
-                            echo 'title: '.$post['title'].'<br/>';
-                            echo 'content: '.$post['content'].'<br/>';
-                        ?>
-                        <h3 class="p-4 text-lg">
-                            Post by :  <?php   echo $post['username'].'<br/>'; ?>
-                        </h3>
+                    <div id="post_<?php echo $post['id'] ;?>" class="transition-[height] ease-in-out h-72 w-auto m-12 bg-darkerViolet border-t-2 border-basicWhite text-basicWhite">
+                        <div class="h-5/6 overflow-hidden" id="content_<?php echo $post['id']; ?>">
+                            <?php    
+                                echo '<div class="pl-4 pt-3 pb-2 text-xl text-lighterWhite bg-gradient-to-r from-basicViolet to-darkerViolet font-extrabold border-b-2 w-1/3 float-left">'.$post['title'].'</div>';
+                                echo '<div class="float-right text-xl font-extrabold pr-4 pt-3 pb-2 border-b-2 bg-gradient-to-l from-basicViolet to-darkerViolet">By : '.$post['username'].'</div><br/>';
+                                echo '<div class="clear-both text-lg px-6 py-4 mb-2 max-h-42">'.$post['content'].'</div><br/>';        
+                            ?>
+                        </div>
+                        <div id="tool_<?php echo post['id']; ?>" class="h-14 border-t-2 bg-basicDark flex items-center justify-end px-8 py-2 text-lg">
+                            <button id="read_<?php echo $post['id'] ?>" onclick="read(<?php echo $post['id'];?>)" class="text-lighterWhite font-extrabold"></button>
+                            <button>
+                        </div>
                     </div>  
+                    <script>
+                        overflow(<?php echo $post['id'] ;?>)
+                    </script>
                 <?php }?>
             </div>
         </section>
-        
     </main>
-<script src="../scripts/info.js"></script>
 </body>
 </html>

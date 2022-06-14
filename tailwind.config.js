b9558d9f68d785035057fe7ce8bd65efe182c216
kornelu123 <kornelu12@gmail.com>
2022-06-14 21:26:54 +0200

    fraction site , styles , php zadanie dlaa cb :3

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,11 +3,20 @@ module.exports = {
   theme: {
     extend: {
       screens: {
-        xs: '370px',
-        sm: '440px',
-        md: '768px',
-        lg: '976px',
-        xl: '1440px',
+        'sm': '640px',
+      // => @media (min-width: 640px) { ... }
+
+      'md': '768px',
+      // => @media (min-width: 768px) { ... }
+
+      'lg': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'xl': '1280px',
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': '1536px',
+      // => @media (min-width: 1536px) { ... }
       },
       colors: {
         basicDark : '#1F2041',
